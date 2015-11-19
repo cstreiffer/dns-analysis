0012f27aedb4ab75dedb9824acd103c3f5b51f47
klb52 <klb52@duke.edu>
2015-11-19 07:51:39 -0500

    Ch-ch-ch-ch-ch-ch-changes

---
+++ dns_client/client/main/query_generator_small.py
@@ -0,0 +1,55 @@
+import time
+import random
+import json
+
+start_time = time.time()
+
+
+weekday = "Fri"
+date = "06"
+month = "Nov"
+year = "2015"
+types = ["A", "AAAA", "MX", "NS", "PTR", "SOA", "SRV", "TXT"]
+domain_ip_file = "dns_ip_map.txt"
+domain_ip_txt = open(domain_ip_file)
+domain_ip_list = domain_ip_txt.read().split('\n')
+
+#out_file = open('dns_queries.txt', 'w')
+counter = 0
+with open('dns_queries_small.txt', 'w') as out_file:
+	for i in range(0,10000):
+		domip_ind = random.randint(0,len(domain_ip_list[0:20])-1)
+		domain_ip = domain_ip_list[domip_ind].split(":")
+		domain = domain_ip[0].strip()
+		ip = domain_ip[1].strip()
+		counter+=1
+		type_ind = random.randint(0,7)
+		dns_type = types[type_ind]
+		hour = random.randint(12,20)
+		minute = random.randint(0,59)
+		second = random.randint(0,59)
+		mac_addr = ""
+		for j in range(0, 12):
+	    		mac_addr = mac_addr+"%x" % random.randint(0, 15) 	
+	    		if(j!=11 and (j+1)%2==0):
+	    			mac_addr=mac_addr+":"
+
+
+
+		datetimestring = weekday+', '+date+' '+month+' '+year+' '+str(hour)+':'+str(minute)+':'+str(second)+' GMT'
+		timestamp = time.mktime(time.strptime(datetimestring, '%a, %d %b %Y %H:%M:%S GMT'))
+		print >> out_file, domain+","+ip+","+str(timestamp)#+","+dns_type+","+mac_addr
+#		json_data = {}
+#		json_data['domain'] = domain
+#		json_data['ip_address'] = ip
+#		json_data['time'] = timestamp
+#		json_data['type'] = dns_type
+#		json_data['MAC'] = mac_addr
+
+#		json.dump(json_data, out_file)
+#		print >> out_file, ""
+		#print counter
+
+out_file.close()
+
+print("--- %s seconds ---" % (time.time() - start_time))
---
+++ ec2_listener/JavaWordCount.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+
+import scala.Tuple2;
+
+public final class JavaWordCount {
+  private static final Pattern SPACE = Pattern.compile(" ");
+
+  public static void main(String[] args) throws Exception {
+
+    if (args.length < 1) {
+      System.err.println("Usage: JavaWordCount <file>");
+      System.exit(1);
+    }
+
+    SparkConf sparkConf = new SparkConf().setAppName("JavaWordCount");
+    JavaSparkContext ctx = new JavaSparkContext(sparkConf);
+    JavaRDD<String> lines = ctx.textFile(args[0], 1);
+
+    JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+      @Override
+      public Iterable<String> call(String s) throws NumberFormatException {
+    	  List<String> query_list = Arrays.asList(s.split("\n"));
+    	  StringBuilder sb = new StringBuilder();
+
+    	  for(String q_s:query_list){
+    		  String domain = q_s.split(",")[0];
+    		  String ip_addr = q_s.split(",")[1];
+    		  String time = q_s.split(",")[2];
+    		  String type = q_s.split(",")[3];
+    		  String MAC = q_s.split(",")[4];
+        	  int time_val = Integer.parseInt(time.split("\\.")[0]);
+        	  time_val = time_val - (time_val%1800);
+        	  time = Integer.toString(time_val);
+        	  sb.append(domain+" ");
+        	  sb.append(ip_addr+" ");
+        	  sb.append(type+" ");
+        	 // sb.append(MAC+" ");
+        	  sb.append(time);
+        	  sb.append("\n");
+        	  System.out.print(sb.toString());
+    		  
+    	  }
+    	  
+        return Arrays.asList(sb.toString().split("\n"));
+      }
+    });
+
+    JavaPairRDD<String, Integer> ones = words.mapToPair(new PairFunction<String, String, Integer>() {
+      @Override
+      public Tuple2<String, Integer> call(String s) {
+        return new Tuple2<String, Integer>(s, 1);
+      }
+    });
+
+    JavaPairRDD<String, Integer> counts = ones.reduceByKey(new Function2<Integer, Integer, Integer>() {
+      @Override
+      public Integer call(Integer i1, Integer i2) {
+        return i1 + i2;
+      }
+    });
+
+    List<Tuple2<String, Integer>> output = counts.collect();
+    for (Tuple2<?,?> tuple : output) {
+      System.out.println(tuple._1() + ": " + tuple._2());
+    }
+    ctx.stop();
+  }
+}
