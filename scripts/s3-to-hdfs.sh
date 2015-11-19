7745c301bfb4e2eb423767163d3ffe73545c41a3
Christopher Streiffer <cds33@duke.edu>
2015-11-19 07:37:24 -0500

    Fixes the script

--- scripts/s3-to-hdfs.sh
+++ scripts/s3-to-hdfs.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
-CUR_DIR=/root/scripts
+CUR_DIR=/root/goodPaulHuntingAJauntIntoDNS/scripts
 S3_DIR=s3://cs-514/temp
 HDFS_DIR=/user/hadoop
 FILE=dns_queries.txt
-LOG_FILE=$CUR_DIR/output-log.txt
+LOG_FILE=$CUR_DIR/../logs/output-log.txt
 
 run() {
 	# Log the action
 	write_to_log "COPYING" "Copying $FILE from s3, moving to hdfs."
 		
 	# Move from s3 to local directory
-	/usr/local/bin/aws s3 cp "${S3_DIR}/${FILE}" $CUR_DIR/
+	/usr/local/bin/aws s3 cp "${S3_DIR}/${FILE}" $CUR_DIR/ > /dev/null 2>&1
 	
 	# Delete the file from s3, catch the output	
-	if [ -n "$(/root/ephemeral-hdfs/bin/hadoop fs -ls "${HDFS_DIR}/${FILE}" | grep -o "$FILE")" ]; then
-		write_to_log "DELETING" "Removing file $FILE from hdfs."
-		/root/ephemeral-hdfs/bin/hadoop fs -rm "${HDFS_DIR}/${FILE}"
-	fi
+	write_to_log "DELETING" "Removing file $FILE from hdfs."
+	/root/ephemeral-hdfs/bin/hadoop fs -rm "${HDFS_DIR}/${FILE}" > /dev/null 2>&1
 
 	# Add the file to hdfs
 	write_to_log "WRITING" "Writing $FILE to hdfs."
-        /root/ephemeral-hdfs/bin/hadoop fs -rm "${CUR_DIR}/${FILE}"
+	/root/ephemeral-hdfs/bin/hadoop fs -put "${CUR_DIR}/${FILE}" "${HDFS_DIR}/${FILE}" > /dev/null 2>&1
 
 	# Clean up the local file
-	rm "${CUR_DIR}/${FILE}"
+	rm "${CUR_DIR}/${FILE}" > /dev/null 2>&1
 }
 
 write_to_log() {
