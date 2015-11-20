22c168c6884d1c86c58acecd48a1bf8565aa9412
Christopher Streiffer <christopherstreiffer@wl-10-190-52-181.wireless.duke.edu>
2015-11-20 07:27:57 -0500

    Does things

--- ec2_listener/client/dns/DNSClientSender.java
+++ ec2_listener/client/dns/DNSClientSender.java
@@ -1,9 +1,9 @@
 package client.dns;
 
 import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ConnectException;
 import java.net.Socket;
 
@@ -20,15 +20,15 @@ public class DNSClientSender {
 	
 	public static void main(String[] args) throws InterruptedException {
 		try {
-			DataOutputStream writer = null;
-			DataInputStream reader = null;
-			Socket client = null;
+			BufferedReader reader = null;
+			PrintWriter writer = null;
+			Socket server = null;
 			BufferedReader textReader = null;
 			try {
-				client = new Socket(IP_ADDRESS, PORT);				
-				writer = new DataOutputStream(client.getOutputStream());
-				reader = new DataInputStream(client.getInputStream());
-				System.out.println("Starting connection from: " + client.getRemoteSocketAddress());
+				server = new Socket(IP_ADDRESS, PORT);				
+				reader = new BufferedReader(new InputStreamReader(server.getInputStream()));			
+			    writer = new PrintWriter(server.getOutputStream(), true);
+				System.out.println("Starting connection from: " + server.getRemoteSocketAddress());
 				
 				FileReader fr = new FileReader(DNS_FILE);
 				textReader = new BufferedReader(fr);
@@ -37,19 +37,22 @@ public class DNSClientSender {
 				String thisLine = "";
 				while ((thisLine = textReader.readLine()) != null) {
 					json_data = thisLine;
-					writer.writeUTF(json_data);
+					writer.println(json_data);
 				}
-				writer.writeUTF(DONE);
+				writer.println(DONE);
 				
-				String input;
-				while(!(input = reader.readUTF()).equals(DONE)){}
+				while((thisLine = reader.readLine()) != null){
+					if(thisLine.equals(DONE)) {
+						break;
+					}
+				}
 				
 			} finally {
-				System.out.println("Ending connection from: " + client.getRemoteSocketAddress());
+				System.out.println("Ending connection from: " + server.getRemoteSocketAddress());
 				Closeables.close(textReader, true);
 				Closeables.close(writer, true);
 				Closeables.close(reader, true);
-				Closeables.close(client, true);
+				Closeables.close(server, true);
 			}
 		} catch (ConnectException ce) {
 			System.err.println("Could not connect: " + ce);
--- ec2_listener/listener/server/EC2ListenServerThread.java
+++ ec2_listener/listener/server/EC2ListenServerThread.java
@@ -1,10 +1,10 @@
 package listener.server;
 
-import java.io.BufferedWriter;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ConnectException;
 import java.net.Socket;
 
@@ -13,7 +13,7 @@ import com.google.common.io.Closeables;
 public class EC2ListenServerThread extends Thread {
 
 	private Socket server;
-	private BufferedWriter myWriter;
+	private FileWriter myWriter;
 	
 	private static final String MNT_FILE = "/mnt/temp/dns_queries.txt";
 	//private static final String MNT_FILE = "output.txt";
@@ -21,30 +21,36 @@ public class EC2ListenServerThread extends Thread {
 	
 	public EC2ListenServerThread(Socket server) throws IOException {
 		this.server = server;
-		this.myWriter = new BufferedWriter(new FileWriter(MNT_FILE, true));
+		this.myWriter = new FileWriter(MNT_FILE, true);
 	}
 	
 	public void run() {
 		try {
 			System.out.println("Just connected to " + server.getRemoteSocketAddress());
-			DataInputStream reader = null;
-			DataOutputStream writer = null;
+			BufferedReader reader = null;
+			PrintWriter writer = null;
+
 			try {
-				reader = new DataInputStream(server.getInputStream());
-				writer = new DataOutputStream(server.getOutputStream());			
-				
+				reader = new BufferedReader(new InputStreamReader(server.getInputStream()));			
+			    writer = new PrintWriter(server.getOutputStream(), true);
+
 				String input = "";
-				while((input = reader.readUTF()) != null && !input.equals(DONE)) {
-					myWriter.write(input);
-					myWriter.newLine();
+				while((input = reader.readLine()) != null) {
+					if(input.equals(DONE)) {
+						break;
+					} else {
+						myWriter.write(String.format("%s\n", input));
+						myWriter.flush();
+					}
 				}		        
-				writer.writeUTF(DONE);
-				
+				writer.println(DONE);
+
 			} finally {
 				System.out.println("Exiting connection: " + server.getRemoteSocketAddress());
 				Closeables.close(writer, true);
 				Closeables.close(reader, true);
 				Closeables.close(server, true);
+				Closeables.close(myWriter, true);
 			}
 		} catch(ConnectException ce) {
 		      System.err.println("Could not connect: " + ce);
