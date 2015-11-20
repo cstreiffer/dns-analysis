03acb41e16e4aa7c943ebe77437a410747ed6e34
klb52 <klb52@duke.edu>
2015-11-20 05:10:08 -0500

    CCCCCCChanges

--- ec2_listener/QueryMapReduce.java
+++ ec2_listener/QueryMapReduce.java
@@ -34,20 +34,22 @@ public final class QueryMapReduce {
     	  StringBuilder sb = new StringBuilder();
 
     	  for(String q_s:query_list){
-    		  String domain = q_s.split(",")[0];
-    		  String ip_addr = q_s.split(",")[1];
-    		  String time = q_s.split(",")[2];
-    		  String type = q_s.split(",")[3];
-    		  String MAC = q_s.split(",")[4];
-        	  int time_val = Integer.parseInt(time.split("\\.")[0]);
-        	  time_val = time_val - (time_val%1800);
-        	  time = Integer.toString(time_val);
-        	  sb.append(domain+" ");
-        	  sb.append(ip_addr+" ");
-        	  //sb.append(type+" ");
-        	 // sb.append(MAC+" ");
-        	  sb.append(time);
-        	  sb.append("\n");    		  
+    		  if(q_s.split(",").length>0){
+	    		  String domain = q_s.split(",")[0];
+	    		  String ip_addr = q_s.split(",")[1];
+	    		  String time = q_s.split(",")[2];
+	    		  String type = q_s.split(",")[3];
+	    		  String MAC = q_s.split(",")[4];
+	        	  int time_val = Integer.parseInt(time.split("\\.")[0]);
+	        	  time_val = time_val - (time_val%1800);
+	        	  time = Integer.toString(time_val);
+	        	  sb.append(domain+" ");
+	        	  sb.append(ip_addr+" ");
+	        	  //sb.append(type+" ");
+	        	 // sb.append(MAC+" ");
+	        	  sb.append(time);
+	        	  sb.append("\n");   
+    		  }
     	  }
     	  
         return Arrays.asList(sb.toString().split("\n"));
@@ -67,7 +69,100 @@ public final class QueryMapReduce {
         return i1 + i2;
       }
     });
+    counts.saveAsTextFile("query_mr_results");
+    
+    
+    
+//    JavaRDD<String> times = lines.flatMap(new FlatMapFunction<String, String>() {
+//        @Override
+//        public Iterable<String> call(String s) throws NumberFormatException {
+//      	  List<String> query_list = Arrays.asList(s.split("\n"));
+//      	  StringBuilder sb = new StringBuilder();
+//
+//      	  for(String q_s:query_list){
+//      		  String domain = q_s.split(",")[0];
+//      		  String ip_addr = q_s.split(",")[1];
+//      		  String time = q_s.split(",")[2];
+//      		  String type = q_s.split(",")[3];
+//      		  String MAC = q_s.split(",")[4];
+//          	  int time_val = Integer.parseInt(time.split("\\.")[0]);
+//          	  time_val = time_val - (time_val%1800);
+//          	  time = Integer.toString(time_val);
+//          	  sb.append(domain+" ");
+//          	  sb.append(ip_addr+" ");
+//          	  //sb.append(type+" ");
+//          	 // sb.append(MAC+" ");
+//          	  sb.append(time);
+//          	  sb.append("\n");    		  
+//      	  }
+//      	  
+//          return Arrays.asList(sb.toString().split("\n"));
+//        }
+//      });
+//
+//      JavaPairRDD<String, Integer> ones = words.mapToPair(new PairFunction<String, String, Integer>() {
+//        @Override
+//        public Tuple2<String, Integer> call(String s) {
+//          return new Tuple2<String, Integer>(s, 1);
+//        }
+//      });
+//
+//      JavaPairRDD<String, Integer> counts = ones.reduceByKey(new Function2<Integer, Integer, Integer>() {
+//        @Override
+//        public Integer call(Integer i1, Integer i2) {
+//          return i1 + i2;
+//        }
+//      });
+//      counts.saveAsTextFile("query_mr_results");
+//    
+//    
+//    
+//    
+//    
+//      JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+//          @Override
+//          public Iterable<String> call(String s) throws NumberFormatException {
+//        	  List<String> query_list = Arrays.asList(s.split("\n"));
+//        	  StringBuilder sb = new StringBuilder();
+//
+//        	  for(String q_s:query_list){
+//        		  String domain = q_s.split(",")[0];
+//        		  String ip_addr = q_s.split(",")[1];
+//        		  String time = q_s.split(",")[2];
+//        		  String type = q_s.split(",")[3];
+//        		  String MAC = q_s.split(",")[4];
+//            	  int time_val = Integer.parseInt(time.split("\\.")[0]);
+//            	  time_val = time_val - (time_val%1800);
+//            	  time = Integer.toString(time_val);
+//            	  sb.append(domain+" ");
+//            	  sb.append(ip_addr+" ");
+//            	  //sb.append(type+" ");
+//            	 // sb.append(MAC+" ");
+//            	  sb.append(time);
+//            	  sb.append("\n");    		  
+//        	  }
+//        	  
+//            return Arrays.asList(sb.toString().split("\n"));
+//          }
+//        });
+//
+//        JavaPairRDD<String, Integer> ones = words.mapToPair(new PairFunction<String, String, Integer>() {
+//          @Override
+//          public Tuple2<String, Integer> call(String s) {
+//            return new Tuple2<String, Integer>(s, 1);
+//          }
+//        });
+//
+//        JavaPairRDD<String, Integer> counts = ones.reduceByKey(new Function2<Integer, Integer, Integer>() {
+//          @Override
+//          public Integer call(Integer i1, Integer i2) {
+//            return i1 + i2;
+//          }
+//        });
+//        counts.saveAsTextFile("query_mr_results");
+    
 
+    
     List<Tuple2<String, Integer>> output = counts.collect();
     for (Tuple2<?,?> tuple : output) {
       System.out.println(tuple._1() + ": " + tuple._2());
